ea96c4ff376efd97404b2b55debd1e24344105cb
devanshshukla99 <devanshshukla99@outlook.com>
2021-11-24 03:07:09 +0530

    update

--- stylesheets/script.js
+++ stylesheets/script.js
@@ -10,3 +10,41 @@ function sidebar(){
         document.querySelector("#sbar").style.transform = "translateX(0%)"
     }
 }
+
+header = `
+<div class="theme-switch-wrapper">
+    <label class="theme-switch" for="checkbox">
+        <input type="checkbox" id="checkbox" />
+        <div class="slider round"></div>
+    </label>
+</div>
+<header>
+    <img src="assets/avatar.png" width="60%" height="60%" style="border-radius: 50%;">
+    <br>
+    <a href="index.html">Home</a>
+    <br>
+    <a href="about.html">About</a>
+    <br>
+    <a href="blogs.html">Blogs</a>
+    <br>
+    <a href="contact.html">Contact</a>
+    <br>
+</header>`;
+
+
+window.onload = function (){
+    var head = document.querySelector("header")
+    head.innerHTML = header
+    var toggleSwitch = document.querySelector(".theme-switch input")
+    function switchTheme(e) {
+        if (e.target.checked) {
+            document.documentElement.setAttribute('data-theme', 'dark');
+        }
+        else {
+            document.documentElement.setAttribute('data-theme', 'light');
+        }    
+    }
+    if (toggleSwitch) {
+        toggleSwitch.addEventListener('change', switchTheme, false);
+    }
+}
